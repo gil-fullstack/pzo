825283ce080830b880aa8fcb19e4f3184d603350
gil-fullstack <gilservicosweb@gmail.com>
2025-08-23 16:51:03 -0300

    update image into index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,15 +3,59 @@
 
 <template>
 <div class="my_main">
-  <h1>Poços Zona Oeste</h1>
+  <div class="banner">
+    <div class="bg"></div>
+  </div>
+  <div class="my_text mt-7">
+    <h1>Região de Crescimento e Prosperidade</h1>
+  </div>
 </div>
 </template>
 
 <style lang="scss" scoped>
-.my_main{
+.my_main {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: flex-start;
   align-items: center;
-  padding: 2rem;
+  padding: 0;
+  height: 100%;
+
+  .banner {
+    width: 100%;
+    height: 100%;
+    min-height: 100vh;
+    min-width: 100vh;
+
+    .bg {
+      position: absolute;
+      background-image: url('../assets/prefeitura5.jpg');
+      background-size: cover;
+      background-position: center;
+      min-height: 100%;
+      max-height: 100%;
+      inset: 0;
+      z-index: 1;
+    }
+  }
+}
+
+@media (max-width: 768px) {
+  .my_main {
+    .banner {
+      min-height: auto;
+      margin-top: 21%;
+      .bg {
+        margin-top: 14%;
+        background-image: url('../assets/prefeitura5.jpg');
+        background-size: 100%;
+        background-position: top;
+        min-height: 50%;
+        max-height: 100%;
+        max-width: 100%;
+        inset: 0;
+      }
+    }
+  }
 }
 </style>
